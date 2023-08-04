c60bb224cc54c2b565255f8fe7bc69cfe163787d
stevetian1227 <s1902684157@gmail.com>
2023-08-05 01:03:04 +0930

    Add the Edit Action and its corressponding view

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -31,7 +31,7 @@ namespace BulkyBookWeb.Controllers
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("Custom Error", "The DisplayOrder cannot exactly match the Name."); 
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name."); 
             }
             if (ModelState.IsValid)
             {
@@ -42,5 +42,41 @@ namespace BulkyBookWeb.Controllers
             return View(obj);
         }
 
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst= _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //var categoryFromDbSingle= _db.Categories.SingleOrDefault(u=>u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
